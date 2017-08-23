562c55bf428f6a806b702f699fbc64d30e8b2c83
Matthew Hotchen <github@mhn.me>
2017-08-23 18:53:41 +0100

    Move to codecraft and require salt in constructor

--- spec/Craftwork/IdObfuscator/IdObfuscatorSpec.php
+++ spec/Craftwork/IdObfuscator/IdObfuscatorSpec.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace spec\LuxPhp\IdObfuscator;
+namespace spec\Craftwork\IdObfuscator;
 
-use LuxPhp\IdObfuscator\IdObfuscator;
+use Craftwork\IdObfuscator\IdObfuscator;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -10,6 +10,7 @@ class IdObfuscatorSpec extends ObjectBehavior
 {
     function it_encodes_an_integer_to_string()
     {
+        $this->beConstructedWith('');
         $this->encode(0)->shouldReturn('a');
         $this->encode(64)->shouldReturn('ba');
         $this->encode(128)->shouldReturn('ca');
@@ -17,6 +18,7 @@ class IdObfuscatorSpec extends ObjectBehavior
 
     function it_decodes_a_string_to_integer()
     {
+        $this->beConstructedWith('');
         $this->decode('a')->shouldReturn(0);
         $this->decode('ba')->shouldReturn(64);
         $this->decode('ca')->shouldReturn(128);
@@ -24,19 +26,21 @@ class IdObfuscatorSpec extends ObjectBehavior
 
     function it_encodes_an_integer_to_string_with_provided_salt()
     {
-        $this->encode(0, 'test')->shouldReturn('L');
-        $this->encode(64, 'test')->shouldReturn('sL');
+        $this->beConstructedWith('test');
+        $this->encode(0)->shouldReturn('L');
+        $this->encode(64)->shouldReturn('sL');
     }
 
     function it_decodes_an_integer_to_string_with_provided_salt()
     {
-        $this->decode('L', 'test')->shouldReturn(0);
-        $this->decode('sL', 'test')->shouldReturn(64);
+        $this->beConstructedWith('test');
+        $this->decode('L')->shouldReturn(0);
+        $this->decode('sL')->shouldReturn(64);
     }
 
     function it_encode_an_integer_with_custom_character_set()
     {
-        $this->beConstructedWith('0123456789abcdef');
+        $this->beConstructedWith('', '0123456789abcdef');
         $this->encode(16)->shouldReturn('10');
     }
 }
